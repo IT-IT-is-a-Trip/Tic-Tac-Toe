79aa0ba019e222b7df2c312c5ada544ca8cc7b9e
IT-IT-is-a-trip <alexey135135025@gmail.com>
2025-04-04 22:50:53 +0300

    created working version of console game tic tac toe with all logic

--- js.js
+++ js.js
@@ -1,7 +1,10 @@
 const GameBoard = (function () {
-    let gameBoard = ['', '', '', '', '', '', '', '', ''];
+    let gameBoard = [
+        '', '', '',
+        '', '', '',
+        '', '', ''];
     const getBoard = () => gameBoard;
-    const markCell = (index, mark) => { gameBoard[index] = mark };
+    const markCell = (index, mark) => { gameBoard[index] = !gameBoard[index] ? mark : gameBoard[index] };
     const resetBoard = () => gameBoard = ['', '', '', '', '', '', '', '', ''];
     return {
         getBoard,
@@ -12,14 +15,18 @@ const GameBoard = (function () {
 const gameController = function (
     playerOneName = 'Player one (X)',
     playerTwoName = 'Player two (O)') {
+    let board = GameBoard.getBoard();
+    let gameEnded = false
     const players = [
         {
             name: playerOneName,
             symbol: "X",
+            wins: 0
         },
         {
             name: playerTwoName,
             symbol: 'O',
+            wins: 0
         }
     ];
     let activePlayer = players[0] //активный игрок по умолчанию
@@ -31,20 +38,68 @@ const gameController = function (
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
     const printRoundDetails = () => {
-        console.log(GameBoard.getBoard());
-        console.log(`${getActivePlayer().name}'s turn`);
+        //создаёт "интерфейс" в консоли
+        console.log(`
+            ${board[0]} | ${board[1]} | ${board[2]}
+           ---------
+            ${board[3]} | ${board[4]} | ${board[5]}
+           ---------
+            ${board[6]} | ${board[7]} | ${board[8]}
+           `);
+        if (!gameEnded) {
+            console.log(`${getActivePlayer().name}'s turn`);
+        }
     }
+
     const playRound = (index) => {
+        if (gameEnded) {
+            console.log(`${activePlayer.name} won`);
+            return;
+        }
         let playerMark = activePlayer.symbol; // метка активного игрока
+        //заполнение ячейки меткой активного игрока:
+        if (board[index]) {
+            return;
+        } else {GameBoard.markCell(index, playerMark)};
+        //проверка победителя или ничьи по паттернам.
+
+        const checkWinner = () => {
+            if (gameEnded) {
+                console.log(`${activePlayer.name} won`);
+                return;
+            };
+            const winPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+            for (const pattern of winPatterns) {
+                const [a, b, c] = pattern;
+                if (board[a] && board[a] == board[b] && board[a] === board[c]) {
+                    gameEnded = true;
+                    console.log(`${activePlayer.name} won`);
+                    activePlayer.wins++;
+                    return;
+                }
+            }
+            for (const i of board) {
+                if (!i) return switchPlayer();
+            }
+            gameEnded = true;
+            printRoundDetails();
+            return console.log(`I's tie`);
+        };
+        checkWinner();
         printRoundDetails();
-        GameBoard.markCell(index, playerMark);
-        switchPlayer();
+    }
+    printRoundDetails();
+    const resetGame = () => {
+        GameBoard.resetBoard();
+        gameEnded = false;
+        board = GameBoard.getBoard();
+        activePlayer = players[0];
         printRoundDetails();
     }
     return {
         playRound,
-        getActivePlayer
+        getActivePlayer,
+        resetGame
     }
 }
 const game = gameController();
-//победные комбинации: 012 345 678 036 147 258 048 246
